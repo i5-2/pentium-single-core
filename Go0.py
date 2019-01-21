e2ef09848f42a62f465eddc205206287c50971ac
Mandy Meindersma <meinders@ualberta.ca>
2019-01-21 10:54:55 -0700

    fixing the go1 rename

--- Go0.py
+++ Go0.py
@@ -12,12 +12,10 @@ from gtp_connection import GtpConnection
 from board_util import GoBoardUtil
 from simple_board import SimpleGoBoard
 
-class Go1():
+class Go0():
     def __init__(self):
         """
-        Go player that selects moves randomly from the set of legal moves.
-        However, it filters eye-filling moves.
-        Passes only if there is no other legal move.
+        Gomoku player that selects moves randomly from the set of legal moves.
 
         Parameters
         ----------
@@ -26,7 +24,7 @@ class Go1():
         version : float
             version number (used by the GTP interface).
         """
-        self.name = "Go1"
+        self.name = "Go0"
         self.version = 1.0
         
     def get_move(self, board, color):
@@ -38,7 +36,7 @@ def run():
     start the gtp connection and wait for commands.
     """
     board = SimpleGoBoard(7)
-    con = GtpConnection(Go1(), board)
+    con = GtpConnection(Go0(), board)
     con.start_connection()
 
 if __name__=='__main__':
