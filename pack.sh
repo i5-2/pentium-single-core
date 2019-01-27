6db435124c9203be33a2caf6c295eb31d6069c88
hreherch <hreherch@ualberta.ca>
2019-01-27 16:39:44 -0700

    i hAtE EverYtHing

--- pack.sh
+++ pack.sh
@@ -1,5 +1,2 @@
 rm -rf assignment1.tgz
-cd ..
-tar cfz assignment1.tgz assignment1/*
-mv assignment1.tgz assignment1
-cd assignment1
+tar cfz assignment1.tgz board_util.py Go0.py gtp_connection.py README simple_board.py test_board_util.py test_simple_board.py presubmission.log
